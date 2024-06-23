7c8fa5221ac2b7604e38bdcb6fc2cffe0b04ed6d
BrandynTucknott <114444356+BrandynTucknott@users.noreply.github.com>
2024-06-23 05:49:28 -0700

    FINALLY DONE
    completed AOC 2015 Day 1

--- intel/aoc/2015/d1.asm
+++ intel/aoc/2015/d1.asm
@@ -5,12 +5,15 @@ section .data
     fclosed db "file has been successfully closed", 0
 
     floor_level_str db "Floor Santa must go to: ", 0
+    basement_pos_str db "First entered the basement at position: ", 0
     FILE_BUFFER_SIZE equ 1000
 
 section .bss
     fd resq 1               ; file descriptor
     fbuffer resb FILE_BUFFER_SIZE       ; 1000 byte buffer
-    floor_level_int resq 1  ; signed integer respresenting how far up/down Santa has to go
+    up_int resq 1
+    down_int resq 1
+    first_in_basement_pos resq 1
 
 
 section .text
@@ -32,11 +35,17 @@ _start:
     mov rdi, fd
     mov [rdi], rax
 
-    WRITE_BUFFER fopened
-    NL
+    ; WRITE_BUFFER fopened
+    ; NL
+
+    
+    ; zero out floor_level
+    mov qword [up_int], 0
+    mov qword [down_int], 0
+    mov qword [first_in_basement_pos], 0
+
 
     ; read from file process
-    mov QWORD [floor_level_int], 0
     _process_loop:
         ; get buffer portion
         ZERO_BUFFER fbuffer, FILE_BUFFER_SIZE
@@ -59,46 +68,86 @@ _start:
             je _inc_count
             
             ; else dec_count bc char == ')'
-            dec QWORD [floor_level_int]
+            inc qword [down_int]
             jmp _loop_prep
 
             _inc_count:
-                inc QWORD [floor_level_int]
+                inc qword [up_int]
             ; prep for next iteration
             _loop_prep:
-                inc rcx
-                cmp rcx, FILE_BUFFER_SIZE
-                jl _parse_buffer_loop
-                jmp _process_loop
+                cmp qword [first_in_basement_pos], 0
+                jg _final_loop_prep_stages
+                ; in basement? check
+                push rax
+                push rbx
+
+                mov rax, [up_int]
+                mov rbx, [down_int]
+                cmp rbx, rax
+                jle _close_basement_check
+
+                ; first in basement, store the position
+                mov rax, [up_int]
+                mov rbx, [down_int]
+                add qword [first_in_basement_pos], rax
+                add qword [first_in_basement_pos], rbx
+                
+                _close_basement_check:
+                    pop rbx
+                    pop rax
+                _final_loop_prep_stages:
+                    inc rcx
+                    cmp rcx, FILE_BUFFER_SIZE
+                    jl _parse_buffer_loop
+                    jmp _process_loop
         ; get a new buffer worth of data
         ; jmp _process_loop
 
     _done_reading_file:     ; print the floor needed to go to
-        WRITE_BUFFER floor_level_str
-
-        mov rax, [floor_level_int]
-        mov rbx, 1 << 63
-        and rax, rbx
-
-        NL
-        WRITE_UINT rbx
-        NL
-        WRITE_UINT rax
-        NL
-
+        ; figure out if ups > downs
+        mov rax, [up_int]
+        mov rbx, [down_int]
+        mov rcx, 0 ; will hold the final value (unsigned): |ups - downs|
+        mov rdx, 0 ; determines if we are printing a negative sign: 0 - no, 1 - yes
 
         cmp rax, rbx
-        mov rax, [floor_level_int]
-        jl _output_positive_count
+        jge _output_positive_count
+
+
+        ; delete this block: prints out num ups and downs
+        ; push rax
+        ; mov rax, [up_int]
+        ; NL
+        ; WRITE_UINT rax
+        ; HYPHEN
+        ; mov rax, [down_int]
+        ; WRITE_UINT rax
+        ; NL
+        ; pop rax
 
         ; negative output
-        HYPHEN
-        dec rax
-        not rax ; convert to actual number from two's complement
+        inc rdx
+        mov rcx, rbx
+        sub rcx, rax
+        jmp _print_floor
 
         ; output positive count
         _output_positive_count:
-            WRITE_UINT rax
+            mov rcx, rax
+            sub rcx, rbx
+
+        ; actually print the stuff here
+        _print_floor:
+            WRITE_BUFFER floor_level_str
+            cmp rdx, 0 ; rdx == 0 ? don't print negative sign : print negative sign
+            jz _just_print
+            HYPHEN
+            _just_print:
+                WRITE_UINT rcx
+                NL
+        _print_basement_pos:
+            WRITE_BUFFER basement_pos_str
+            WRITE_UINT [first_in_basement_pos]
             NL
 
 
@@ -107,7 +156,7 @@ _start:
     mov rdi, [fd]
     syscall
 
-    WRITE_BUFFER fclosed
-    NL
+    ; WRITE_BUFFER fclosed
+    ; NL
 
     EXIT 0
